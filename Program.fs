efcc6eca4cee8d96b4ec2e5ab03651d82557fa6d
Matt March <matthewmarch29@gmail.com>
2020-08-20 18:44:20 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let matchDay day = 
+  match day with
+    | "1" -> Day1.solve
+    | _ -> printfn "Input doesn't seem to match any days"
+
+
+[<EntryPoint>]
+let main argv =
+  matchDay argv.[0]
+  0 // return an integer exit code
+ 
