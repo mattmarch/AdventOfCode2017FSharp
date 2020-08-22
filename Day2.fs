836b7963ec3d979135c554c0fd1cb2e94c226619
Matt March <matthewmarch29@gmail.com>
2020-08-22 09:58:23 +0100

    Complete day 2 part 2

--- Day2.fs
+++ Day2.fs
@@ -6,5 +6,19 @@ let input = File.ReadLines("Day2.txt") |> Seq.map (fun line -> (line.Split '\t')
 
 let lineDifference line = (Seq.max line) - (Seq.min line)
 
+type ExactDivisionResult = ExactlyDivisible of int | NotDivisible
+
+let exactDivision a b = 
+  match a % b with
+    | 0 -> Some (a / b)
+    | _ -> None
+
+let lineWithout x line = line |> Seq.filter (fun y -> y <> x)
+
+let matchingDivisor line x = line |> lineWithout x |> Seq.tryPick (exactDivision x)
+
+let lineDivision line = line |> Seq.pick (matchingDivisor line)
+
 let solve spreadsheet =
   printfn "Part 1: %i" (spreadsheet |> Seq.sumBy lineDifference)
+  printfn "Part 2: %i" (spreadsheet |> Seq.sumBy lineDivision)
